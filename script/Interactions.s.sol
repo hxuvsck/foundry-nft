eb1d162fd58fb011194bef09d28e6d7429c2f40b
hxuvsck <hxuvsck@gmail.com>
2025-12-18 12:53:58 +0800

    Interactions.s.sol for nft

---
+++ script/Interactions.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+
+contract MintBasicNft is Script {
+    string public constant PUG =
+        "https://bafybeicdlctvdhgvhnu5xqjm6tvjzaw3oyllq77deguvllb52hzu3ur76m.ipfs.dweb.link?filename=pug.png";
+
+    function run() external {
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment(
+            "BasicNft",
+            block.chainid
+        );
+        mintNftOnContract(mostRecentDeployed);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNft(contractAddress).mintNft(PUG);
+        vm.stopBroadcast();
+    }
+}
