d5694f04752c383efd074174b4ba03f941c50a48
hxuvsck <hxuvsck@gmail.com>
2026-01-04 01:46:34 +0800

    Added natspect with importing openzeppelin erc721 and changed the name of sol file to MoodNft.sol

---
+++ src/MoodNft.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title Mood NFT for course learning that is going to be deployed on chain by SVG file
+ * @author Khuslen Ganbat
+ * @notice This is for learning purpose only
+ */
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MoodNft {}
