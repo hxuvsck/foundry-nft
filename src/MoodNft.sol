bd8869f3dc4d33d7d7fd023231e693b074b0c5e7
hxuvsck <hxuvsck@gmail.com>
2026-01-04 22:40:58 +0800

    Added minting function with tokenURI viewer function

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -31,5 +31,23 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MoodNft is ERC721 {
-    constructor() ERC721("Mood Nft", "MN") {}
+    constructor(
+        uint256 private s_tokenCounter;
+        string private s_sadSvg;
+        string private s_happySvg;
+
+        string memory sadSvg,
+        string memory happySvg
+    ) ERC721("Mood Nft", "MN") {
+        s_tokenCounter = 0;
+        s_sadSvg = sadSvg;
+        s_happySvg = happySvg;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns(string memory) {}
 }
