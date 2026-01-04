fd5e95950d998960bc2879e2a65d861501b3a6a6
hxuvsck <hxuvsck@gmail.com>
2026-01-04 22:33:45 +0800

    Added contract layout on MoodNft.sol and implemented constructor of ERC721 as name and symbol + added happy.svg file in img folder

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -1,3 +1,24 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
@@ -9,4 +30,6 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MoodNft {}
+contract MoodNft is ERC721 {
+    constructor() ERC721("Mood Nft", "MN") {}
+}
