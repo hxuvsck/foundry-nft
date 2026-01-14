a71dce9f036272c025ac4bd8ff206bf4d543b0be
hxuvsck <hxuvsck@gmail.com>
2026-01-14 21:13:17 +0800

    svg to ImageURL function has added to deployment section

--- script/DeployMoodNft.s.sol
+++ script/DeployMoodNft.s.sol
@@ -24,9 +24,21 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {MoodNft} from "../src/MoodNft.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract DeployMoodNft is Script {
     function run() public returns (MoodNft) {
         // We could hard code our SVG files as shown in test of MoodNftTest.t.sol, but Patrick showed more option which added function below.
+    }
+
+    function svgToImageURI(
+        string memory svg
+    ) public view returns (string memory) {
+        // What this function actually do is
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+        // return string(abi.encodePacked(baseURL, svgBase64Encoded)); // Could use string.concat. Which one would be more gas efficient
     }
 }
