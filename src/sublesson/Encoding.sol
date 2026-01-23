6967add38d802f0b6e1bb955afac1e0c1974e4e9
hxuvsck <hxuvsck@gmail.com>
2026-01-23 20:36:08 +0800

    Starting Opcode sublesson of Advanced Foundry on Remix IDE

---
+++ src/sublesson/Encoding.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title Sublesson for Opcodes of EVM
+ * @author Khuslen Ganbat (lead by Patrick Collings)
+ * @notice I think this might not need to be in this NFT file section as to check it out on Remix IDE, whereas it might use gas for this sample NFT course lesson.
+ */
+
+contract Encoding {
+
+}
